8b313bce1abb489d70d71ced1fcbd6c52d92534a
Tereza Dankova <104557274+TerezaDankova@users.noreply.github.com>
2022-08-21 23:36:36 +0200

    adding js

---
+++ index.js
@@ -0,0 +1,16 @@
+//12) Create a button next to each section to collapse and reopen the given section
+
+$('#myCollapsible').shown.bs.collapse({
+  toggle: false
+})
+
+
+//13) Create a method that counts the number of “travels” on the page
+
+//14) Create a modal view with a message and an email field
+
+//15) When the user clicks on “Contact us” the modal is displayed
+
+//16) Create a function that adds a “HOT” badge to all the “Welcome Summer” offers
+
+//17) Create a function to delete all the “cards” from the page
